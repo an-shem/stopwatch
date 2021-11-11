99fbd66a7fdfeac5e74283c6ab5a7271c3998b53
Andrey Shemet <an-shem@ukr.net>
2021-11-11 20:12:54 +0200

    Подчистил файлы стилей

--- src/components/AppStopwatch/App.styled.jsx
+++ src/components/AppStopwatch/App.styled.jsx
@@ -1,7 +1,6 @@
 import styled from '@emotion/styled';
 
 export const AppWrap = styled.div`
-  /* padding: 150px; */
   display: flex;
   justify-content: center;
   flex-direction: column;
@@ -13,7 +12,6 @@ export const AppWrap = styled.div`
   border: 1px solid #c9c1c1;
   border-radius: 50%;
   box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.37) inset;
-  /* box-shadow: inset 2px 2px 5px rgba(154, 147, 140, 0.5), 1px 1px 5px rgba(255, 255, 255, 1); */
 `;
 
 export const WrapperButton = styled.div`
